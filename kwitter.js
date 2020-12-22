66fc8c6e14df1e7bf76e92971b4b57ef358a65c4
PRATYUSH-ROY <66131007+PRATYUSH-ROY@users.noreply.github.com>
2020-12-22 14:14:43 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,20 +1,10 @@
-window.onload = function() {
-    var firebaseConfig = {
-        apiKey: "AIzaSyC7NKPbLgUYgtwCKexC97I9f-5GNzhrrfI",
-        authDomain: "tkwitter-66728.firebaseapp.com",
-        projectId: "tkwitter-66728",
-        storageBucket: "tkwitter-66728.appspot.com",
-        messagingSenderId: "1090902628477",
-        appId: "1:1090902628477:web:0b809acd97a080605a7914",
-        measurementId: "G-8R88Q1EC8F"
-    };
 
-    firebase.initializeApp(firebaseConfig);
+function addUser() {
+
+  user_name = document.getElementById("user_name").value;
+
+  localStorage.setItem("user_name", user_name);
+  
+    window.location = "kwitter_room.html";
 }
 
-function adduser() {
-    var name = document.getElementById("user_name").value;
-    firebase.database().ref("/").child(name).update({
-        purpose: "adding room name"
-    });
-}
